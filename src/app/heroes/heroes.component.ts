14bd5cafc929704bd5d873de86b174cd74d1ec66
teopenna <matteo.penna@outlook.com>
2021-03-09 22:27:09 +0100

    Initial load

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
